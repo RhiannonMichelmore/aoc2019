c1e4692aaed29a51151d4d2ed93b22449448f711
Rhiannon Michelmore <rhiannon.michelmore@gmail.com>
2019-12-10 08:54:48 +0000

    day 9

---
+++ 9/2.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.insert(1,'/home/rhiba/aoc2019/utils')
+import intcode
+
+def main(in_string):
+    ops = list(map(int,in_string.split(',')))
+
+    position = 0
+    relbase = 0
+    inputs = [2]
+    outputs = []
+
+    while position >= 0:
+        position, relbase = intcode.operate(position,relbase,ops,inputs,outputs)
+
+    print(outputs)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('No input found.')
+    else:
+        with open(sys.argv[1],'r') as f:
+            in_string = f.read().strip()
+            main(in_string)
--- utils/intcode.py
+++ utils/intcode.py
@@ -1,34 +1,57 @@
 import time
 # takes current position and list of ops, returns new position 
-def operate(position,ops,inputs=[],outputs=[],threaded=False):
+
+def get_position(param_mode, index, ops, relbase):
+    if param_mode == '0':
+        return ops[index]
+    elif param_mode == '1':
+        return index
+    elif param_mode == '2':
+        return relbase + ops[index]
+
+def bounds(pos,ops):
+    if pos >= 0 and pos < len(ops):
+        return ops[pos]
+    else:
+        return 0
+
+def write(pos,ops,value):
+    if pos >= len(ops):
+        # write to new memory location
+        # get dist to new location
+        n_append = pos - (len(ops)-1)
+        for i in range(n_append):
+            ops.append(0)
+        ops[pos] = value
+    else:
+        ops[pos] = value
+
+def operate(position,relbase,ops,inputs=[],outputs=[],threaded=False):
     instruction = ops[position]
     opcode = int(str(instruction)[-2:])
     param_modes = str(instruction)[:-2]
 
     if opcode == 99:
         # as 99 is halt
-        return -1
+        return -1, -1
     elif opcode == 1:
         # Addition
         # 3 params, pad param_modes array then reverse it
         param_modes = '0'*(3-len(param_modes)) + param_modes
         param_modes = param_modes[::-1]
 
-        if param_modes[0] == '0':
-            first = ops[ops[position+1]]
-        elif param_modes[0] == '1':
-            first = ops[position+1]
+        pos = get_position(param_modes[0],position+1,ops,relbase)
+        first = bounds(pos,ops)
 
-        if param_modes[1] == '0':
-            second = ops[ops[position+2]]
-        elif param_modes[1] == '1':
-            second = ops[position+2]
+        pos = get_position(param_modes[1],position+2,ops,relbase)
+        second = bounds(pos,ops)
 
         plus = first + second
 
-        # dont need to check param mode here since it'll always be position mode
-        ops[ops[position+3]] = plus
-        return position + 4
+        to_write_pos = get_position(param_modes[2],position+3,ops,relbase)
+        write(to_write_pos,ops,plus)
+
+        return position + 4, relbase
 
     elif opcode == 2:
         # Multiplication
@@ -36,21 +59,17 @@ def operate(position,ops,inputs=[],outputs=[],threaded=False):
         param_modes = '0'*(3-len(param_modes)) + param_modes
         param_modes = param_modes[::-1]
 
-        if param_modes[0] == '0':
-            first = ops[ops[position+1]]
-        elif param_modes[0] == '1':
-            first = ops[position+1]
+        pos = get_position(param_modes[0],position+1,ops,relbase)
+        first = bounds(pos,ops)
 
-        if param_modes[1] == '0':
-            second = ops[ops[position+2]]
-        elif param_modes[1] == '1':
-            second = ops[position+2]
+        pos = get_position(param_modes[1],position+2,ops,relbase)
+        second = bounds(pos,ops)
 
         mult = first * second
 
-        # dont need to check param mode here since it'll always be position mode
-        ops[ops[position+3]] = mult
-        return position + 4
+        to_write_pos = get_position(param_modes[2],position+3,ops,relbase)
+        write(to_write_pos,ops,mult)
+        return position + 4, relbase
 
     elif opcode == 3:
         # Input and store, (dont need to check param mode)
@@ -59,62 +78,55 @@ def operate(position,ops,inputs=[],outputs=[],threaded=False):
                 pass
         input_val = inputs.pop(0)
 
-        ops[ops[position+1]] = input_val
+        param_modes = '0'*(1-len(param_modes)) + param_modes
+        to_write_pos = get_position(param_modes[0],position+1,ops,relbase)
 
-        return position + 2
+        write(to_write_pos,ops,input_val)
+
+        return position + 2, relbase
 
     elif opcode == 4:
         # Output
         param_modes = '0'*(1-len(param_modes)) + param_modes
         param_modes = param_modes[::-1]
 
-        if param_modes[0] == '0':
-            out_val = ops[ops[position+1]]
-        elif param_modes[0] == '1':
-            out_val = ops[position+1]
+        pos = get_position(param_modes[0],position+1,ops,relbase)
+        out_val = bounds(pos,ops)
 
         outputs.append(out_val)
-        return position + 2
+        return position + 2, relbase
 
     elif opcode == 5:
         # Jump if true
         param_modes = '0'*(2-len(param_modes)) + param_modes
         param_modes = param_modes[::-1]
 
-        if param_modes[0] == '0':
-            determine_jump = ops[ops[position+1]]
-        elif param_modes[0] == '1':
-            determine_jump = ops[position+1]
+        pos = get_position(param_modes[0],position+1,ops,relbase)
+        determine_jump = bounds(pos,ops)
 
-        if param_modes[1] == '0':
-            new_ip = ops[ops[position+2]]
-        elif param_modes[1] == '1':
-            new_ip = ops[position+2]
+        pos = get_position(param_modes[1],position+2,ops,relbase)
+        new_ip = bounds(pos,ops)
 
         if not determine_jump == 0:
-            return new_ip
+            return new_ip, relbase
         else:
-            return position + 3
+            return position + 3, relbase
 
     elif opcode == 6:
         # Jump if false
         param_modes = '0'*(2-len(param_modes)) + param_modes
         param_modes = param_modes[::-1]
 
-        if param_modes[0] == '0':
-            determine_jump = ops[ops[position+1]]
-        elif param_modes[0] == '1':
-            determine_jump = ops[position+1]
+        pos = get_position(param_modes[0],position+1,ops,relbase)
+        determine_jump = bounds(pos,ops)
 
-        if param_modes[1] == '0':
-            new_ip = ops[ops[position+2]]
-        elif param_modes[1] == '1':
-            new_ip = ops[position+2]
+        pos = get_position(param_modes[1],position+2,ops,relbase)
+        new_ip = bounds(pos,ops)
 
         if determine_jump == 0:
-            return new_ip
+            return new_ip, relbase
         else:
-            return position + 3
+            return position + 3, relbase
 
     elif opcode == 7:
         # Less than
@@ -122,24 +134,21 @@ def operate(position,ops,inputs=[],outputs=[],threaded=False):
         param_modes = '0'*(3-len(param_modes)) + param_modes
         param_modes = param_modes[::-1]
 
-        if param_modes[0] == '0':
-            first = ops[ops[position+1]]
-        elif param_modes[0] == '1':
-            first = ops[position+1]
+        pos = get_position(param_modes[0],position+1,ops,relbase)
+        first = bounds(pos,ops)
 
-        if param_modes[1] == '0':
-            second = ops[ops[position+2]]
-        elif param_modes[1] == '1':
-            second = ops[position+2]
-
+        pos = get_position(param_modes[1],position+2,ops,relbase)
+        second = bounds(pos,ops)
 
         if first < second:
             to_store = 1
         else:
             to_store = 0
-        # dont need to check param mode here since it'll always be position mode
-        ops[ops[position+3]] = to_store
-        return position + 4
+
+        to_write_pos = get_position(param_modes[2],position+3,ops,relbase)
+        write(to_write_pos,ops,to_store)
+
+        return position + 4, relbase
 
     elif opcode == 8:
         # Equals
@@ -147,21 +156,26 @@ def operate(position,ops,inputs=[],outputs=[],threaded=False):
         param_modes = '0'*(3-len(param_modes)) + param_modes
         param_modes = param_modes[::-1]
 
-        if param_modes[0] == '0':
-            first = ops[ops[position+1]]
-        elif param_modes[0] == '1':
-            first = ops[position+1]
+        pos = get_position(param_modes[0],position+1,ops,relbase)
+        first = bounds(pos,ops)
 
-        if param_modes[1] == '0':
-            second = ops[ops[position+2]]
-        elif param_modes[1] == '1':
-            second = ops[position+2]
-
+        pos = get_position(param_modes[1],position+2,ops,relbase)
+        second = bounds(pos,ops)
 
         if first == second:
             to_store = 1
         else:
             to_store = 0
-        # dont need to check param mode here since it'll always be position mode
-        ops[ops[position+3]] = to_store
-        return position + 4
+
+        to_write_pos = get_position(param_modes[2],position+3,ops,relbase)
+        write(to_write_pos,ops,to_store)
+        return position + 4, relbase
+
+    elif opcode == 9:
+        param_modes = '0'*(1-len(param_modes)) + param_modes
+        param_modes = param_modes[::-1]
+
+        pos = get_position(param_modes[0],position+1,ops,relbase)
+        relbase_move = bounds(pos,ops)
+
+        return position + 2, relbase+relbase_move
